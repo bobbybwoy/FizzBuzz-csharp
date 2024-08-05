3218d046dc25d3ee6e0d95c58c7f4f1ff6df1d17
Robert De Leon <68812513+bobbybwoy@users.noreply.github.com>
2024-08-05 12:01:09 +0100

    Added new files

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿for (int i = 1; i <= 10000; i++)
+{
+    string text = "";
+
+    if (i % 3 == 0)
+        text += "Fizz";
+
+    if (i % 13 == 0)
+        text += "Fezz";
+
+    if (i % 5 == 0)
+        text += "Buzz";
+
+    if (i % 7 == 0)
+        text += "Bang";
+
+    if (i % 11 == 0)
+    {
+        text = "Bong";
+        if (i % 13 == 0)
+            text = "FezzBong";
+    }
+
+    if (i % 17 == 0)
+    {
+        if (text.Length == 8)
+        {
+            text = text.Substring(4, 4) + text.Substring(0, 4);
+        }
+        else if (text.Length == 12)
+        {
+            text = text.Substring(8, 4) + text.Substring(4, 4) + text.Substring(0, 4);
+        }
+    }
+
+    if (text != "")
+        Console.WriteLine(text);
+    else
+        Console.WriteLine(i);
+}
